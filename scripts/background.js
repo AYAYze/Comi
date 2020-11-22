1be7f454d3b87836e9923933644fd6ec64647a29
unknown <ehdud2004@gmail.com>
2020-11-22 15:02:23 +0900

    Popup and add

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,7 @@
 //SWITCH ON / OFF
 let status = true;
 
+/*
 chrome.browserAction.onClicked.addListener((tabs)=>{
     status = !status;
     chrome.tabs.sendMessage(tabs.id ,{switch:status});
@@ -18,3 +19,38 @@ chrome.browserAction.onClicked.addListener((tabs)=>{
         });
 })
 //
+
+*/
+
+function shutdown(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        status = !status;
+        chrome.tabs.sendMessage(tabs[0].id, {switch:status});
+        if(!status){
+            chrome.browserAction.setIcon({
+                path : {
+                    "16": "../icons/16_gray.png"
+                }
+            });
+            document.getElementById("shutdown").value = "켜기";
+        }
+        else if(status){
+            chrome.browserAction.setIcon({
+                path : {
+                    "16" : "../icons/16.png"
+                }
+            });
+            document.getElementById("shutdown").value = "끄기";
+        }
+    });
+    
+}
+document.getElementById("shutdown").onclick = shutdown;
+
+
+function startWrite(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id, {write: true});
+    });
+}
+document.getElementById("write").onclick = startWrite;
--- scripts/content.js
+++ scripts/content.js
@@ -8,14 +8,25 @@ let data = fetch('http://localhost:80/comments/', {
 }).then(res =>
     res.json()
 ).then(json => {
-    console.log(json);
+    console.log(json.ang);
 });
 
+function setXY(event) {
+    console.log(event.pageX, event.pageY);
+    document.body.removeEventListener("click", setXY)
+}
 
 chrome.runtime.onMessage.addListener((message,sender) => {
-    if(!message.switch) {
+    if(message.switch == false) {
         console.log(message.switch, "종료합니다.");
-    } else if(message.switch) {
+    } else if(message.switch == true) {
         console.log(message.switch, "시작합니다.");
     }
+    
+    //쓰기
+    if(message.write == true) {
+        console.log('ON');
+        document.body.addEventListener("click", setXY, false);
+    }
 });
+
